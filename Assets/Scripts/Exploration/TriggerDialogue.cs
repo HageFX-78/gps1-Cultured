8a63acc64714f393b1c46b38b2484947ad8334ed
Hage <89595749+HageFX-78@users.noreply.github.com>
2022-06-17 23:44:57 +0800

    Stage layout + Interactable dialogue trigger
    Stage layout was added according to the design docs, with some expansion and modification. Doors/gate and switch placeholders are added.
    The trigger dialogue has a new checkbox call InteractableType, if true, it can only trigger dialogue when player is collided with it and press space

--- Assets/Scripts/Exploration/TriggerDialogue.cs
+++ Assets/Scripts/Exploration/TriggerDialogue.cs
@@ -6,13 +6,42 @@ public class TriggerDialogue : MonoBehaviour
 {
     [SerializeField] TextAsset convoFile;
     [SerializeField] DialogueManager manager;
+    [SerializeField] bool interactableType;
+    [SerializeField] bool convoTriggered;//False means dialogue never triggered, true means triggered alr and wont trigger again. False by default
+    bool inRange = false, interacting = false;
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player") && !convoTriggered)
         {
-            manager.startConversation(convoFile);
+            if(!interactableType)
+            {
+                convoTriggered = true;
+                manager.startConversation(convoFile);
+            }
+            else
+            {
+                inRange = true;
+            }
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            inRange = false;
+            interacting = false;
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space) && interactableType && inRange && !interacting)
+        {
+            interacting = true;
+            manager.startConversation(convoFile);          
         }
     }
 }
