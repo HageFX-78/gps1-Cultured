9e4e46eddab56d9f1203ff010fb3278c6e0d06af
Alex Mah Jia Yong <alexmah1234@gmail.com>
2022-07-20 00:54:01 +0800

    test push

--- Assets/Scripts/Battle/Boss/BossEmotionManager.cs
+++ Assets/Scripts/Battle/Boss/BossEmotionManager.cs
@@ -5,9 +5,80 @@ using TMPro;
 
 public class BossEmotionManager : MonoBehaviour
 {
-    public Emotion[] emotionList;
-    public EmotionManager emotionManager;
+
+
+    [Header("Boss Emotion (Phases)")]
+    [SerializeReference] private string[] bossPhases;
+    public Emotion emotion = new();
+
+    [Header("Emotion Bar Details")]
     public RectTransform PosBar, NegBar, SafeZone;
     public TextMeshProUGUI positiveEmotionTxt, enemyEmotionTxt;
 
+    private int index;
+
+    private void Start()
+    {
+        index = 0;
+        emotion.currentType = bossPhases[index];
+        Debug.Log(emotion.currentType);
+        index++;
+
+        emotion.currentType = bossPhases[index];
+        Debug.Log(emotion.currentType);
+    }
+
+    public void InitialiseType() //checks the enemy current type, and then has the corresponding multipliers
+    {
+        if (emotion.currentType == "Delusional")
+        {
+            emotion.TypeMultiplier = new Dictionary<string, float>()
+            {
+                {"Rationality", 1.5f},
+                {"Love", 1.0f},
+                {"Hope", 1.0f},
+                {"Acceptance", -1.0f}
+            };
+        }
+        else if (emotion.currentType == "Hatred")
+        {
+            emotion.TypeMultiplier = new Dictionary<string, float>()
+            {
+                {"Rationality", 1.0f},
+                {"Love", -1.0f},
+                {"Hope", 1.0f},
+                {"Acceptance", 1.5f}
+            };
+        }
+        else if (emotion.currentType == "Self_Loathing")
+        {
+            emotion.TypeMultiplier = new Dictionary<string, float>()
+            {
+                {"Rationality", 1.0f},
+                {"Love", 1.5f},
+                {"Hope", -1.0f},
+                {"Acceptance", 1.0f}
+            };
+        }
+        else if (emotion.currentType == "Despair")
+        {
+            emotion.TypeMultiplier = new Dictionary<string, float>()
+            {
+                {"Rationality", -1.0f},
+                {"Love", 1.0f},
+                {"Hope", 1.5f},
+                {"Acceptance", 1.0f}
+            };
+        }
+        else if (emotion.currentType == "Righteousness")
+        {
+            emotion.TypeMultiplier = new Dictionary<string, float>()
+            {
+                {"Rationality", 1.5f},
+                {"Love", -1.0f},
+                {"Hope", 1.0f},
+                {"Acceptance", 1.0f}
+            };
+        }
+    }
 }
