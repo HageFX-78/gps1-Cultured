6ff2c198eaffe66a577c0a5f62906490850f4a8d
Alex Mah Jia Yong <alexmah1234@gmail.com>
2022-08-04 02:57:33 +0800

    take my changes

--- Assets/Scripts/Exploration/Props/RenderPlayerLamppost.cs
+++ Assets/Scripts/Exploration/Props/RenderPlayerLamppost.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RenderingPlayer : MonoBehaviour
+public class RenderPlayerLamppost : MonoBehaviour
 {
 
     [SerializeField] SpriteRenderer sr;
