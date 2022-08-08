905b83863b0deca926cad9b806a05ec401e05fdd
Hage <89595749+HageFX-78@users.noreply.github.com>
2022-08-09 02:25:44 +0800

    Tutorial deco and sprites

--- Assets/Scripts/UI&Environment/Tutorial/Interactables.cs
+++ Assets/Scripts/UI&Environment/Tutorial/Interactables.cs
@@ -7,13 +7,23 @@ public class Interactables : MonoBehaviour
     public bool onTop;
     public GameObject door;
 
+    public SpriteRenderer thisSR;
+    public Sprite onSprite;
+
+    private void Start()
+    {
+        thisSR = gameObject.GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
+
         if (onTop)
         {
             if(Input.GetKeyDown(KeyCode.Space))
             {
                 door.SetActive(false);
+                thisSR.sprite = onSprite;
+                thisSR.flipX = false;
             }
 
         }
